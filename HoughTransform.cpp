028208477af450c19516f5c1ba36de671ed59cf7
side_realms <87800512+doiuec@users.noreply.github.com>
2022-04-05 11:40:00 +0900

    Create HoughTransform.cpp

---
+++ HoughTransform.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#pragma comment( lib, "opencv_world455d.lib" )
+
+int main(int argc, const char* argv[])
+{
+    int hr = -1;
+
+    try
+    {
+        cv::Mat src, edge, dst;
+        std::vector<cv::Vec4i> lines;
+
+        src = cv::imread("C:\\Users\\oslab\\Documents\\laser_mean\\beehive\\2.5.png", cv::IMREAD_GRAYSCALE);
+
+        cv::namedWindow("src", 1);
+        imshow("src", src);
+
+        cv::Canny(src, edge, 50, 200, 3);
+
+        dst = cv::Mat::zeros(src.rows, src.cols, CV_8UC3);
+
+        int fromTo[] = { 0, 2, 0, 1, 0, 0 };
+
+        cv::mixChannels(&src, 1, &dst, 1, fromTo, 3);
+
+        cv::HoughLinesP(
+            edge,           
+            lines,          
+            1,              
+            CV_PI / 180.0,  
+            80,             
+            30,             
+            10              
+        );
+
+        for (auto line : lines)
+        {
+            cv::line(dst, cv::Point(line[0], line[1]), cv::Point(line[2], line[3]), cv::Scalar(0, 0, 255), 1);
+        }
+
+        cv::namedWindow("dst", 1);
+        cv::imshow("dst", dst);
+
+        cv::waitKey(0);
+
+        hr = 0;
+    }
+
+    catch (cv::Exception ex)
+    {
+        std::cout << ex.err << std::endl;
+    }
+
+    cv::destroyAllWindows();
+
+    return hr;
+}
